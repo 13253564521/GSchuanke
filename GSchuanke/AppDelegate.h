90cda15c990a46e91d66d0dfcd5310af0989b4ff
刘高升 <liu695880140@icloud.com>
2016-11-18 11:43:36 +0800

    Initial Commit

---
+++ GSchuanke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSchuanke
+//
+//  Created by 刘高升 on 16/11/18.
+//  Copyright © 2016年 刘高升. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
